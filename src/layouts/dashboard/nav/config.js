983dde6635cf97862e1652df5e073a405392f163
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-04-24 21:32:26 +0200

    Modify navigation

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -27,12 +27,72 @@ const navConfig = [
     icon: icon('ic_blog'),
   },
   {
-    title: 'login',
+    title: 'Strona główna',
+    path: '/dashboard/mainPage',
+    icon: icon('insights_black_24dp'),
+  },
+  {
+    title: 'Pieśni',
+    path: '/dashboard/songs',
+    icon: icon('library_music_black_24dp'),
+  },
+  {
+    title: 'Zarządzanie pieśniami',
+    path: '/dashboard/songManager',
+    icon: icon('lyrics_black_24dp'),
+  },
+  {
+    title: 'Playlisty',
+    path: '/dashboard/playlists',
+    icon: icon('queue_music_black_24dp'),
+  },
+  {
+    title: 'Tworzenie playlist',
+    path: '/dashboard/playlistCreate',
+    icon: icon('playlist_add_black_24dp'),
+  },
+  {
+    title: 'Kategorie',
+    path: '/dashboard/categories',
+    icon: icon('category_black_24dp'),
+  },
+  {
+    title: 'Profile',
+    path: '/dashboard/profiles',
+    icon: icon('portrait_black_24dp'),
+  },
+  {
+    title: 'Ustawienia',
+    path: '/dashboard/settings',
+    icon: icon('settings_black_24dp'),
+  },
+  {
+    title: 'Kontakt',
+    path: '/dashboard/contacts',
+    icon: icon('chat_bubble_black_24dp'),
+  },
+  {
+    title: 'Użytkownicy',
+    path: '/dashboard/users',
+    icon: icon('people_black_24dp'),
+  },
+  {
+    title: 'Grupy',
+    path: '/dashboard/groups',
+    icon: icon('groups_black_24dp'),
+  },
+  {
+    title: 'Logowanie',
     path: '/login',
     icon: icon('ic_lock'),
   },
   {
-    title: 'Not found',
+      title: 'Odzyskiwanie hasła',
+      path: '/passwordRecovery',
+      icon: icon('ic_lock'),
+    },
+  {
+    title: 'Error 404',
     path: '/404',
     icon: icon('ic_disabled'),
   },
