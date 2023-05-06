d9e5b965416ee09773e9464b29a0c7715773aefd
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-05-06 11:56:09 +0200

    Populating Users list and Role select from backend on Users page #27 (#28)

--- src/sections/@dashboard/users/UserList.js
+++ src/sections/@dashboard/users/UserList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -10,6 +10,17 @@ import SelectType from "./SelectType";
 
 export default function CheckboxList() {
   const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/dashboard/users")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   const handleToggle = (value) => () => {
     // logika
   };
@@ -30,16 +41,19 @@ return (
           overflow: "auto",
         }}
       >
-        {[0, 1, 2, 3, 4, 5, 6].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
+        {data.map((item) => {
+          const labelId = `checkbox-list-label-${item.id}`;
 
-          if (searchText && !(`Line item ${value + 1}`.toLowerCase().includes(searchText.toLowerCase()))) {
+          if (
+            searchText &&
+            !item.name.toLowerCase().includes(searchText.toLowerCase())
+          ) {
             return null;
           }
 
           return (
             <ListItem
-              key={value}
+              key={item.id}
               secondaryAction={
                 <IconButton edge="end" aria-label="delete">
                   <DeleteIcon />
@@ -47,8 +61,11 @@ return (
               }
               sx={{ p: 0 }}
             >
-              <ListItemButton role={undefined} onClick={handleToggle(value)}>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              <ListItemButton
+                role={undefined}
+                onClick={handleToggle(item.id)}
+              >
+                <ListItemText id={labelId} primary={item.name} />
               </ListItemButton>
             </ListItem>
           );
