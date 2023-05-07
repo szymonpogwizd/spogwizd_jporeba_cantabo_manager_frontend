4b4b58208327e8b156da857dd3e4d4a258dad29a
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-05-07 16:10:12 +0200

    Populating Songs list and Category select from backend on Songs page (#33)

--- src/sections/@dashboard/songs/SongList.js
+++ src/sections/@dashboard/songs/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -10,9 +10,42 @@ import SelectCategory from "./SelectCategory";
 
 export default function CheckboxList() {
   const [searchText, setSearchText] = useState("");
-  const handleToggle = (value) => () => {
-    // logika
+    const [data, setData] = useState([]);
+    const [itemToDelete, setItemToDelete] = useState(null);
+
+    useEffect(() => {
+      fetch("http://localhost:8080/dashboard/songs")
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+
+        if (itemToDelete !== null) {
+          setData((prevState) => {
+            const index = prevState.findIndex((item) => item.id === itemToDelete);
+            if (index !== -1) {
+              prevState.splice(index, 1);
+              return [...prevState];
+            }
+            return prevState;
+          });
+          setItemToDelete(null);
+        }
+      });
+  }, [itemToDelete]);
+
+  const handleDelete = (value) => () => {
+    fetch(`http://localhost:8080/dashboard/songs/${value}`, { method: "DELETE" })
+      .then(() => {
+        setItemToDelete(value);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
+
+  const handleToggle = (value) => () => {
+    // logika do zrobienia
+  }
 
   const handleSearch = (newSearchText) => {
     setSearchText(newSearchText);
@@ -30,25 +63,35 @@ return (
           overflow: "auto",
         }}
       >
-        {[0, 1, 2, 3, 4, 5, 6].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
+        {data.map((item) => {
+          const labelId = `checkbox-list-label-${item.id}`;
 
-          if (searchText && !(`Line item ${value + 1}`.toLowerCase().includes(searchText.toLowerCase()))) {
+          if (
+            searchText &&
+            !item.name.toLowerCase().includes(searchText.toLowerCase())
+          ) {
             return null;
           }
 
           return (
             <ListItem
-              key={value}
+              key={item.id}
               secondaryAction={
-                <IconButton edge="end" aria-label="delete">
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={handleDelete(item.id)}
+                >
                   <DeleteIcon />
                 </IconButton>
               }
               sx={{ p: 0 }}
             >
-              <ListItemButton role={undefined} onClick={handleToggle(value)}>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              <ListItemButton
+                role={undefined}
+                onClick={handleToggle(item.id)}
+              >
+                <ListItemText id={labelId} primary={item.name} />
               </ListItemButton>
             </ListItem>
           );
