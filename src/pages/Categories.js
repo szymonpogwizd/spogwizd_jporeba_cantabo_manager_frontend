5e1a6c92563467d7a604dde147ca33fb69fc055c
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-05-17 19:32:51 +0200

    Categories - create, delete, show (#48)

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,18 +1,118 @@
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { useTheme } from '@mui/material/styles';
 import { Grid, Container, Typography } from '@mui/material';
 // sections
 import {
-  FloatingActionButtonsAdd,
   FloatingActionButtonsSave,
-  CategoryList,
-  TextFieldName,
+  PlaylistCategoryList,
+  SongCategoryList,
+  TextFieldNamePlaylistCategories,
+  TextFieldNameSongCategories,
+  AlertMessage,
 } from '../sections/@dashboard/categories';
 // ----------------------------------------------------------------------
 
 export default function Categories() {
   const theme = useTheme();
+    const [nameSongCategoryValue, setNameSongCategoryValue] = useState("");
+    const [namePlaylistCategoryValue, setNamePlaylistCategoryValue] = useState("");
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+    const [successAlertMessage, setSuccessAlertMessage] = useState("");
+
+     const handleSaveSongCategoryClick = () => {
+
+    const resetFormSongCategories = () => {
+      setNameSongCategoryValue("");
+    };
+
+      const dataSongCategories = {
+        name: nameSongCategoryValue,
+      };
+
+        fetch("http://localhost:8080/dashboard/songCategories", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataSongCategories),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              return response.text().then((errorText) => {
+                throw new Error(errorText);
+              });
+            }
+              setSuccessAlertMessage(`Pomyślnie utworzono kategorie pieśni ${nameSongCategoryValue}`);
+              setShowSuccessAlert(true);
+              resetFormSongCategories();
+              return response.json();
+          })
+          .catch((error) => {
+            setAlertMessage(`${error.message}`);
+            setShowAlert(true);
+          });
+      };
+
+      const handleSavePlaylistCategoryClick = () => {
+
+      const resetFormPlaylistCategories = () => {
+        setNamePlaylistCategoryValue("");
+      };
+
+        const dataPlaylistCategories = {
+          name: namePlaylistCategoryValue,
+        };
+
+          fetch("http://localhost:8080/dashboard/playlistCategories", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataPlaylistCategories),
+          })
+            .then((response) => {
+              if (!response.ok) {
+                return response.text().then((errorText) => {
+                  throw new Error(errorText);
+                });
+              }
+                setSuccessAlertMessage(`Pomyślnie utworzono kategorie playlisty ${namePlaylistCategoryValue}`);
+                setShowSuccessAlert(true);
+                resetFormPlaylistCategories();
+                return response.json();
+            })
+            .catch((error) => {
+              setAlertMessage(`${error.message}`);
+              setShowAlert(true);
+            });
+        };
+
+
+    const handleSongCategoryNameChange = (event) => {
+      const value = event.target.value;
+      setNameSongCategoryValue(value);
+    }
+
+    const handlePlaylistCategoryNameChange = (event) => {
+      const value = event.target.value;
+      setNamePlaylistCategoryValue(value);
+    }
+
+    const handleCloseAlert = () => {
+      setShowAlert(false);
+    };
+
+    const handleCloseSuccessAlert = () => {
+      setShowSuccessAlert(false);
+    };
+
+    const resetAlert = () => {
+      setAlertMessage("");
+    };
 
   return (
     <>
@@ -20,37 +120,62 @@ export default function Categories() {
         <title> Kategorie | Cantabo Manager </title>
       </Helmet>
 
+      {showAlert && (
+        <AlertMessage
+          severity="error"
+          title="Błąd"
+          message={alertMessage}
+          onClose={handleCloseAlert}
+          resetAlert={resetAlert}
+        />
+      )}
+
+      {showSuccessAlert && (
+        <AlertMessage
+          severity="success"
+          title="Sukces"
+          message={successAlertMessage}
+          onClose={handleCloseSuccessAlert}
+          resetAlert={resetAlert}
+        />
+      )}
+
       <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Kategorie
-        </Typography>
 
         <Grid container spacing={10}>
 
           <Grid item xs={12} sm={6}>
             {/* Lewa strona */}
+            <Typography variant="h4" sx={{ mb: 5 }}>
+              Kategorie pieśni
+            </Typography>
             <Grid>
               <Grid item xs={12}>
-                {/* Pierwszy element */}
-                <CategoryList />
+                <SongCategoryList />
               </Grid>
               <Grid item xs={12}>
-                {/* Drugi element */}
-                <FloatingActionButtonsAdd />
+                <TextFieldNameSongCategories onChange={handleSongCategoryNameChange} value={nameSongCategoryValue} />
+              </Grid>
+              <Grid item xs={12}>
+                <FloatingActionButtonsSave onClick={handleSaveSongCategoryClick} />
               </Grid>
             </Grid>
           </Grid>
 
           <Grid item xs={12} sm={6}>
             {/* Prawa strona */}
+            <Typography variant="h4" sx={{ mb: 5 }}>
+              Kategorie playlist
+            </Typography>
             <Grid>
               <Grid item xs={12}>
-                {/* Trzeci element */}
-                <TextFieldName />
+                <PlaylistCategoryList />
               </Grid>
               <Grid item xs={12}>
-                {/* Czwarty element */}
-                <FloatingActionButtonsSave />
+                <TextFieldNamePlaylistCategories onChange={handlePlaylistCategoryNameChange} value={namePlaylistCategoryValue} />
+              </Grid>
+              <Grid item xs={12}>
+                <FloatingActionButtonsSave onClick={handleSavePlaylistCategoryClick} />
               </Grid>
             </Grid>
           </Grid>
