022ba1a0d387b38836a2be8d050750c18ab5b0ae
Jporebaa <jakubporeba@gmail.com>
2023-05-19 15:35:11 +0200

    Create SettingsPage

---
+++ src/pages/Settings.js
@@ -0,0 +1,60 @@
+import { Helmet } from 'react-helmet-async';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Grid, Container, Typography } from '@mui/material';
+// sections
+import {
+TextFieldName,
+FloatingActionButtonsSave,
+SwitchActive,
+SetPassword,
+SliderMargin,
+} from '../sections/@dashboard/settings';
+
+
+export default function Ustawienia() {
+return (
+    <>
+      <Helmet>
+        <title> Ustawienia | Cantabo Manager </title>
+      </Helmet>
+
+      <Container maxWidth="xl">
+           <Typography variant="h4" sx={{ mb: 5 }}>
+            Ustawienia
+            </Typography>
+
+            <Grid container spacing ={10}>
+
+                <Grid item xs ={12} sm={6}>
+                 {/* Lewa strona */}
+                 <Grid>
+                        <Grid item xs={12}>
+                        <SwitchActive />
+                        </Grid>
+                        <Grid item xs={12}>
+                        <SliderMargin />
+                        </Grid>
+                        <Grid item xs={12}>
+                        <SetPassword />
+                         </Grid>
+
+                        <Grid>
+                        <FloatingActionButtonsSave />
+                        </Grid>
+
+                    </Grid>
+                 </Grid>
+
+                  <Grid item xs={12} sm={6}>
+                  {/* Prawa strona */}
+
+                  </Grid>
+
+
+            </Grid>
+
+      </Container>
+    </>
+  );
+}
---
+++ src/sections/@dashboard/settings/index.js
@@ -0,0 +1,6 @@
+export { default as TextFieldName } from './TextFieldName';
+export { default as FloatingActionButtonsSave } from '../common/FloatingActionButtonsSave';
+export {default as SetPassword } from './SetPassword';
+export { default as SwitchActive } from './SwitchActive';
+export { default as SliderMargin } from './SliderMargin';
+
