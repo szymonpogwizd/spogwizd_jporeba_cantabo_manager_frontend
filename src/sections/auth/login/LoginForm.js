6b0cfbf883664f6d2eb145a34dc0600cc63bd98a
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-05-19 22:44:44 +0200

    Login configuration (#58)

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,34 +1,91 @@
 import { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-// @mui
 import { Link, Stack, IconButton, Typography, InputAdornment, TextField, Checkbox } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-// components
 import Iconify from '../../../components/iconify';
-
-// ----------------------------------------------------------------------
+import AlertMessage from '../../@dashboard/common/AlertMessage';
 
 export default function LoginForm() {
   const navigate = useNavigate();
-
+  const [usernameValue, setUsername] = useState('');
+  const [passwordValue, setPassword] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [errorCount, setErrorCount] = useState(0);
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClick = () => {
-    navigate('/dashboard', { replace: true });
+  const handleGoToPasswordRecovery = () => {
+    navigate('/passwordRecovery');
   };
 
-   const handleGoToPasswordRecovery = () => {
-     navigate('/passwordRecovery');
-   };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async () => {
+    try {
+      const params = new URLSearchParams();
+      params.append('username', usernameValue);
+      params.append('password', passwordValue);
+
+      const response = await axios.post('http://127.0.0.1:8080/login', params);
+
+      if (response.status === 200) {
+        if (response.data.includes('Invalid credentials')) {
+           handleCloseAlert();
+           setErrorCount(prevCount => prevCount + 1);
+           setAlertMessage(`[${errorCount}] Błędne dane logowania`);
+           setShowAlert(true);
+        } else {
+          navigate('/dashboard', { replace: true });
+        }
+      } else {
+        handleCloseAlert();
+        setErrorCount(prevCount => prevCount + 1);
+        setAlertMessage(`[${errorCount}] Błąd logowania`);
+        setShowAlert(true);
+      }
+    } catch (error) {
+      handleCloseAlert();
+      setErrorCount(prevCount => prevCount + 1);
+      setAlertMessage(`[${errorCount}] Błąd żądania logowania: ${error.message}`);
+      setShowAlert(true);
+    }
+  };
+
+      const handleCloseAlert = () => {
+        setShowAlert(false);
+      };
+
+      const resetAlert = () => {
+        setAlertMessage("");
+      };
 
   return (
     <>
+      {showAlert && (
+        <AlertMessage
+          severity="error"
+          title="Błąd"
+          message={alertMessage}
+          onClose={handleCloseAlert}
+          resetAlert={resetAlert}
+        />
+      )}
+
       <Stack spacing={3}>
-        <TextField name="email" label="Adres email" />
+        <TextField name="username" label="Nazwa użytkownika" value={usernameValue} onChange={handleUsernameChange} />
 
         <TextField
           name="password"
           label="Hasło"
+          value={passwordValue}
+          onChange={handlePasswordChange}
           type={showPassword ? 'text' : 'password'}
           InputProps={{
             endAdornment: (
@@ -43,20 +100,21 @@ export default function LoginForm() {
       </Stack>
 
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-      <Stack direction="row" alignItems="center" spacing={0}>
-        <Checkbox name="remember" label="Remember me"/>
-        <Typography variant="body2">Zapamiętaj mnie</Typography>
+        <Stack direction="row" alignItems="center" spacing={0}>
+          <Checkbox name="remember" />
+          <Typography variant="body2">Zapamiętaj mnie</Typography>
+        </Stack>
+        <Link
+          variant="subtitle2"
+          underline="hover"
+          style={{ cursor: 'pointer' }}
+          onClick={handleGoToPasswordRecovery}
+        >
+          Nie pamiętasz hasła?
+        </Link>
       </Stack>
-      <Link
-        variant="subtitle2"
-        underline="hover"
-        style={{cursor: 'pointer' }}
-        onClick={handleGoToPasswordRecovery}>
-        Nie pamiętasz hasła?
-      </Link>
-    </Stack>
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleLogin}>
         Zaloguj
       </LoadingButton>
     </>
