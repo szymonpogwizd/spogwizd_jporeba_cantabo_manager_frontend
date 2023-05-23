67692bc3c95013f0d7cc26bba81000cb7cacd730
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-05-23 20:51:48 +0200

    Add token expiration check and logout functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 // routes
@@ -11,6 +12,49 @@ import ScrollToTop from './components/scroll-to-top';
 // ----------------------------------------------------------------------
 
 export default function App() {
+  const [logoutReason, setLogoutReason] = useState('');
+
+  useEffect(() => {
+    const isTokenExpired = (token) => {
+      // Implementacja logiki sprawdzania ważności tokenu
+      // ...
+    };
+
+    const checkTokenValidity = () => {
+      const token = localStorage.getItem('token');
+      if (isTokenExpired(token)) {
+        // Wylogowanie użytkownika
+        localStorage.removeItem('token');
+        setLogoutReason('Sesja wygasła.'); // Ustawienie przyczyny wylogowania
+        // Przekierowanie na stronę logowania lub inna akcja
+        window.location.href = '/login'; // Przykładowe przekierowanie na stronę logowania
+      } else {
+        const { exp } = JSON.parse(atob(token.split('.')[1]));
+        const timeToExpiration = exp * 1000 - Date.now();
+
+        if (timeToExpiration <= 90000) { // Mniej niż 90 sekund do wygaśnięcia
+          // Wylogowanie użytkownika
+          localStorage.removeItem('token');
+          setLogoutReason('Sesja wygasa za mniej niż 90 sekund.'); // Ustawienie przyczyny wylogowania
+          // Przekierowanie na stronę logowania lub inna akcja
+          window.location.href = '/login'; // Przykładowe przekierowanie na stronę logowania
+        }
+      }
+    };
+
+    const tokenCheckInterval = setInterval(checkTokenValidity, 1000 * 60); // Sprawdzaj co minutę
+
+    return () => {
+      clearInterval(tokenCheckInterval); // Czyszczenie interwału po zakończeniu komponentu
+    };
+  }, []);
+
+  useEffect(() => {
+    if (logoutReason) {
+      alert(logoutReason); // Wyświetlenie powiadomienia o przyczynie wylogowania
+    }
+  }, [logoutReason]);
+
   return (
     <HelmetProvider>
       <BrowserRouter>
