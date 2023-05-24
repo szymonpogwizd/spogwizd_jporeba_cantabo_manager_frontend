6c91d2a774e29f51cc023ef19e18ca59d4d17a24
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-05-24 19:46:00 +0200

    Add playlist create functionality (#84)

--- src/pages/PlaylistCreate.js
+++ src/pages/PlaylistCreate.js
@@ -31,7 +31,6 @@ export default function PlaylistCreate() {
         </Typography>
 
         <TransferList />
-        <FloatingActionButtonsSave />
 
       </Container>
     </>
--- src/sections/@dashboard/playlistCreate/TransferList.js
+++ src/sections/@dashboard/playlistCreate/TransferList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -10,7 +10,9 @@ import Paper from "@mui/material/Paper";
 import SearchField from '../songs/SearchField';
 import SelectCategory from '../songs/SelectCategory';
 import TextFieldName from './TextFieldName';
-import SelectPlaylistCategory from './SelectPlaylistCategory';
+import CheckboxCategories from './CheckboxCategories';
+import FloatingActionButtonsSave from '../common/FloatingActionButtonsSave';
+import AlertMessage from '../common/AlertMessage';
 
 function not(a, b) {
   return a.filter((value) => b.indexOf(value) === -1);
@@ -22,25 +24,34 @@ function intersection(a, b) {
 
 export default function TransferList() {
   const [checked, setChecked] = useState([]);
-  const [left, setLeft] = useState([0, 1, 2, 3]);
-  const [right, setRight] = useState([4, 5, 6, 7]);
+  const [left, setLeft] = useState([]);
+  const [right, setRight] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   const leftChecked = intersection(checked, left);
   const rightChecked = intersection(checked, right);
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+  const [nameValue, setNameValue] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [successAlertMessage, setSuccessAlertMessage] = useState("");
+  const [refreshKey, setRefreshKey] = useState(0);
+  const [errorCount, setErrorCount] = useState(0);
+  const [selectedCategories, setSelectedCategories] = useState([]);
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
+const handleToggle = (value) => () => {
+  const currentIndex = checked.indexOf(value);
+  const newChecked = [...checked];
 
-    setChecked(newChecked);
-  };
+  if (currentIndex === -1) {
+    newChecked.push(value);
+  } else {
+    newChecked.splice(currentIndex, 1);
+  }
+
+  setChecked(newChecked);
+};
 
   const handleCheckedRight = () => {
     setRight(right.concat(leftChecked));
@@ -54,25 +65,110 @@ export default function TransferList() {
     setChecked(not(checked, rightChecked));
   };
 
+  const fetchData = () => {
+    const headers = {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    };
+
+    return fetch("http://localhost:8080/dashboard/songs", { headers })
+      .then((response) => response.json())
+      .then((data) => {
+        setLeft(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+   const handleSaveClick = () => {
+
+        const resetForm = () => {
+          setNameValue("");
+          setSelectedCategories([]);
+          setRight([]);
+        };
+
+        const data = {
+          name: nameValue,
+          playlistCategories: selectedCategories,
+          songs: right,
+        };
+
+          fetch("http://localhost:8080/dashboard/playlistCreate", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(data),
+          })
+            .then((response) => {
+              if (!response.ok) {
+                return response.text().then((errorText) => {
+                  throw new Error(errorText);
+                });
+              }
+              handleCloseAlert();
+              setSuccessAlertMessage(`Pomyślnie utworzono playliste ${nameValue}`);
+              setShowSuccessAlert(true);
+              resetForm();
+              fetchData().then(() => {
+                setRefreshKey(prevKey => prevKey + 1);
+              });
+              return response.json();
+            })
+            .catch((error) => {
+              handleCloseSuccessAlert();
+              setErrorCount(prevCount => prevCount + 1);
+              setAlertMessage(`[${errorCount}] ${error.message}`);
+              setShowAlert(true);
+            });
+        };
+
+    const handleNameChange = (event) => {
+      const value = event.target.value;
+      setNameValue(value);
+    }
+
+    const handleCloseAlert = () => {
+      setShowAlert(false);
+    };
+
+    const handleCloseSuccessAlert = () => {
+      setShowSuccessAlert(false);
+    };
+
+    const resetAlert = () => {
+      setAlertMessage("");
+    };
+
   const handleSearch = (newSearchText) => {
     setSearchText(newSearchText);
   };
 
+  const handleCategoryChange = (newCategories) => {
+    setSelectedCategories(newCategories);
+  };
+
   const filteredList = left.filter((value) =>
     searchText !== ""
-      ? `List item ${value + 1}`.toLowerCase().includes(searchText.toLowerCase())
+      ? value.toLowerCase().includes(searchText.toLowerCase())
       : true
   );
 
   const customList = (items) => (
     <Paper sx={{ width: "100%", height: 640, overflow: "auto" }}>
       <List dense component="div" role="list">
-        {items.map((value) => {
-          const labelId = `transfer-list-item-${value}-label`;
+        {items.map((value, index) => {
+          const labelId = `transfer-list-item-${index}-label`;
 
           return (
             <ListItem
-              key={value}
+              key={value.id}
               role="listitem"
               button
               onClick={handleToggle(value)}
@@ -87,7 +183,7 @@ export default function TransferList() {
                   }}
                 />
               </ListItemIcon>
-              <ListItemText id={labelId} primary={`List item ${value + 1}`} />
+              <ListItemText id={labelId} primary={value.name} />
             </ListItem>
           );
         })}
@@ -97,41 +193,65 @@ export default function TransferList() {
 
   return (
     <Grid container spacing={2} justifyContent="center" alignItems="center">
+      {showAlert && (
+      <AlertMessage
+        severity="error"
+        title="Błąd"
+        message={alertMessage}
+        onClose={handleCloseAlert}
+        resetAlert={resetAlert}
+      />
+      )}
+
+      {showSuccessAlert && (
+      <AlertMessage
+        severity="success"
+        title="Sukces"
+        message={successAlertMessage}
+        onClose={handleCloseSuccessAlert}
+        resetAlert={resetAlert}
+      />
+      )}
       <Grid item xs={12} md={5.5}>
         <SearchField handleSearch={handleSearch} />
         <SelectCategory />
         {customList(filteredList)}
       </Grid>
-     <Grid item xs={12} md={1} sx={{ textAlign: "center" }}>
-       <Grid container direction="column" alignItems="center">
-         <Button
-           sx={{ my: 0.5 }}
-           variant="contained"
-           color="primary"
-           size="small"
-           onClick={handleCheckedRight}
-           disabled={leftChecked.length === 0}
-           aria-label="move selected right"
-         >
-           &gt;
-         </Button>
-         <Button
-           sx={{ my: 0.5 }}
-           variant="contained"
-           color="primary"
-           size="small"
-           onClick={handleCheckedLeft}
-           disabled={rightChecked.length === 0}
-           aria-label="move selected left"
-         >
-           &lt;
-         </Button>
-       </Grid>
-     </Grid>
+      <Grid item xs={12} md={1} sx={{ textAlign: "center" }}>
+        <Grid container direction="column" alignItems="center">
+          <Button
+            sx={{ my: 0.5 }}
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={handleCheckedRight}
+            disabled={leftChecked.length === 0}
+            aria-label="move selected right"
+          >
+            &gt;
+          </Button>
+          <Button
+            sx={{ my: 0.5 }}
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={handleCheckedLeft}
+            disabled={rightChecked.length === 0}
+            aria-label="move selected left"
+          >
+            &lt;
+          </Button>
+        </Grid>
+      </Grid>
       <Grid item xs={12} md={5.5}>
-        <TextFieldName />
-        <SelectPlaylistCategory />
+        <TextFieldName onChange={handleNameChange} value={nameValue} />
+        <CheckboxCategories onChange={handleCategoryChange} value={selectedCategories} />
         {customList(right)}
+      </Grid>
+      <Grid container justifyContent="flex-end">
+        <Grid item>
+           <FloatingActionButtonsSave onClick={handleSaveClick}/>
+        </Grid>
       </Grid>
     </Grid>
   );
