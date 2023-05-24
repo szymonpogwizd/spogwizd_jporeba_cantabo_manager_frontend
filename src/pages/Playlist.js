3b8d1e91223f49c1430d667b690a551fdf29feaa
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-05-24 21:14:20 +0200

    refactor code

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { useTheme } from '@mui/material/styles';
@@ -11,11 +12,17 @@ import {
    FloatingActionButtonsSave,
    FloatingActionButtonsAdd,
    PlaylistList,
-
+   CheckboxCategories,
 } from '../sections/@dashboard/playlists';
 
 
 export default function Playlist() {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const handleCategoryChange = (newCategories) => {
+    setSelectedCategories(newCategories);
+  };
+
 return (
     <>
       <Helmet>
@@ -26,28 +33,30 @@ return (
            <Typography variant="h4" sx={{ mb: 5 }}>
             Playlisty
             </Typography>
-                <Grid container spacing ={10}>
-                    <Grid item xs ={12} sm={6}>
-                         {/* Lewa strona */}
-                        <Grid>
-                            <PlaylistList/>
-                        </Grid>
-                        <Grid>
-                            <FloatingActionButtonsAdd />
-                        </Grid>
+            <Grid container spacing ={10}>
+                <Grid item xs ={12} sm={6}>
+                     {/* Lewa strona */}
+                    <Grid>
+                        <PlaylistList/>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                         {/* Prawa strona */}
+                    <Grid>
+                        <FloatingActionButtonsAdd />
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                     {/* Prawa strona */}
+                    <Grid>
+                        <Grid item xs={12}>
+                            <TextFieldName />
+                            <CheckboxCategories onChange={handleCategoryChange} value={selectedCategories} />
+                            <PlaylistEditList />
+                        </Grid>
                         <Grid>
-                            <Grid item xs={12}>
-                                <PlaylistEditList />
-                            </Grid>
-                            <Grid>
-                                <FloatingActionButtonsSave />
-                            </Grid>
+                            <FloatingActionButtonsSave />
                         </Grid>
                     </Grid>
                 </Grid>
+            </Grid>
       </Container>
     </>
   );
--- src/sections/@dashboard/playlists/PlaylistEditList.js
+++ src/sections/@dashboard/playlists/PlaylistEditList.js
@@ -1,68 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
 
-import CheckboxCategories from "./CheckboxCategories";
-import TextFieldName from "./TextFieldName"
-
-export default function CheckboxList() {
-  const [searchText, setSearchText] = useState("");
+export default function EmptyList() {
   const [data, setData] = useState([]);
-  const [itemToDelete, setItemToDelete] = useState(null);
-  const [selectedCategories, setSelectedCategories] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/dashboard/playlists")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-
-        if (itemToDelete !== null) {
-          setData((prevState) => {
-            const index = prevState.findIndex((item) => item.id === itemToDelete);
-            if (index !== -1) {
-              prevState.splice(index, 1);
-              return [...prevState];
-            }
-            return prevState;
-          });
-          setItemToDelete(null);
-        }
-      });
-  }, [itemToDelete]);
-
-  const handleDelete = (value) => () => {
-    console.log(value);
-    fetch(`http://localhost:8080/dashboard/playlists/${value}`, { method: "DELETE" })
-      .then(() => {
-        setItemToDelete(value);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const handleToggle = (value) => () => {
-    // logika do zrobienia
-  }
-
-  const handleSearch = (newSearchText) => {
-    setSearchText(newSearchText);
-  };
-
-  const handleCategoryChange = (newCategories) => {
-    setSelectedCategories(newCategories);
-  };
-
-return (
+  return (
     <div>
-      <TextFieldName handleSearch={handleSearch} />
-        <CheckboxCategories onChange={handleCategoryChange} value={selectedCategories} />
       <List
         sx={{
           width: "100%",
@@ -71,39 +14,9 @@ return (
           overflow: "auto",
         }}
       >
-        {data.map((item) => {
-          const labelId = `checkbox-list-label-${item.id}`;
-
-          if (
-            searchText &&
-            !item.name.toLowerCase().includes(searchText.toLowerCase())
-          ) {
-            return null;
-          }
-
-          return (
-            <ListItem
-              key={item.id}
-              secondaryAction={
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={handleDelete(item.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              }
-              sx={{ p: 0 }}
-            >
-              <ListItemButton
-                role={undefined}
-                onClick={handleToggle(item.id)}
-              >
-                <ListItemText id={labelId} primary={item.name} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
+        {data.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
       </List>
     </div>
   );
