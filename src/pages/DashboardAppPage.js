951451671b6297af89ae1bdb4001814e4ca38d36
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-05-27 16:10:28 +0200

    remove unused

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -8,46 +8,45 @@ import { Grid, Container, Typography } from '@mui/material';
 import Iconify from '../components/iconify';
 // sections
 import {
-  AppCurrentVisits,
   AppWidgetSummary,
 } from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
-  const theme = useTheme();
-  const [numSongs, setNumSongs] = useState(0);
-  const [numPlaylists, setNumPlaylists] = useState(0);
-  const [numGroups, setNumGroups] = useState(0);
-  const [numUsers, setNumUsers] = useState(0);
+    const theme = useTheme();
+    const [numSongs, setNumSongs] = useState(0);
+    const [numPlaylists, setNumPlaylists] = useState(0);
+    const [numGroups, setNumGroups] = useState(0);
+    const [numUsers, setNumUsers] = useState(0);
 
-const headers = {
-    Authorization: `Bearer ${localStorage.getItem('token')}`
-};
+    const headers = {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+    };
 
-useEffect(() => {
-    fetch("http://localhost:8080/dashboard/app/users", { headers })
-        .then((response) => response.json())
-        .then((data) => setNumUsers(data || "0"));
-}, []);
+    useEffect(() => {
+        fetch("http://localhost:8080/dashboard/app/users", { headers })
+            .then((response) => response.json())
+            .then((data) => setNumUsers(data || "0"));
+    }, []);
 
-useEffect(() => {
-    fetch("http://localhost:8080/dashboard/app/songs", { headers })
-        .then((response) => response.json())
-        .then((data) => setNumSongs(data || "0"));
-}, []);
+    useEffect(() => {
+        fetch("http://localhost:8080/dashboard/app/songs", { headers })
+            .then((response) => response.json())
+            .then((data) => setNumSongs(data || "0"));
+    }, []);
 
-useEffect(() => {
-    fetch("http://localhost:8080/dashboard/app/playlists", { headers })
-        .then((response) => response.json())
-        .then((data) => setNumPlaylists(data || "0"));
-}, []);
+    useEffect(() => {
+        fetch("http://localhost:8080/dashboard/app/playlists", { headers })
+            .then((response) => response.json())
+            .then((data) => setNumPlaylists(data || "0"));
+    }, []);
 
-useEffect(() => {
-    fetch("http://localhost:8080/dashboard/app/groups", { headers })
-        .then((response) => response.json())
-        .then((data) => setNumGroups(data || "0"));
-}, []);
+    useEffect(() => {
+        fetch("http://localhost:8080/dashboard/app/groups", { headers })
+            .then((response) => response.json())
+            .then((data) => setNumGroups(data || "0"));
+    }, []);
 
   return (
     <>
@@ -76,41 +75,6 @@ useEffect(() => {
           <Grid item xs={12} sm={6} md={3}>
             <AppWidgetSummary title="Grupy" total={numGroups} color="error" icon={'ant-design:group-outlined'} />
           </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Current Visits"
-              chartData={[
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.info.main,
-                theme.palette.warning.main,
-                theme.palette.error.main,
-              ]}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-              <AppCurrentVisits
-                title="Current Visits"
-                chartData={[
-                  { label: 'America', value: 4344 },
-                  { label: 'Asia', value: 5435 },
-                  { label: 'Europe', value: 1443 },
-                  { label: 'Africa', value: 4443 },
-                ]}
-                chartColors={[
-                  theme.palette.primary.main,
-                  theme.palette.info.main,
-                  theme.palette.warning.main,
-                  theme.palette.error.main,
-                ]}
-              />
-            </Grid>
         </Grid>
       </Container>
     </>
--- src/sections/@dashboard/app/AppWidgetSummary.js
+++ src/sections/@dashboard/app/AppWidgetSummary.js
@@ -53,7 +53,7 @@ export default function AppWidgetSummary({ title, total, icon, color = 'primary'
             )} 100%)`,
         }}
       >
-        <Iconify icon={icon} width={24} height={24} />
+        <Iconify icon={icon} width={32} height={32} />
       </StyledIcon>
 
       <Typography variant="h3">{fShortenNumber(total)}</Typography>
