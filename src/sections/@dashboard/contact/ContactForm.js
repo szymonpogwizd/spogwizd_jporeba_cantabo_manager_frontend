8fe853c9afdf77539171981f6ac59302d3716ba1
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-05-31 12:31:54 +0200

    Edit Contact page

--- src/sections/@dashboard/contact/ContactForm.js
+++ src/sections/@dashboard/contact/ContactForm.js
@@ -1,66 +1,58 @@
-import React, { useState, useEffect } from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
+import React from "react";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
-import SelectType from "./SelectType";
-import TextFieldName from "./TextFieldName"
-import TextFieldDescribe from "./TextFieldDescribe"
+const handleOpenMailAppSzymon = (email) => {
+  window.location.href = `mailto:${email}`;
+};
 
-export default function CheckboxList() {
-  const [searchText, setSearchText] = useState("");
-  const [data, setData] = useState([]);
-  const [itemToDelete, setItemToDelete] = useState(null);
+const handleOpenMailAppJakub = (email) => {
+  window.location.href = `mailto:${email}`;
+};
 
-  useEffect(() => {
-    fetch("http://localhost:8080/dashboard/contact")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-
-        if (itemToDelete !== null) {
-          setData((prevState) => {
-            const index = prevState.findIndex((item) => item.id === itemToDelete);
-            if (index !== -1) {
-              prevState.splice(index, 1);
-              return [...prevState];
-            }
-            return prevState;
-          });
-          setItemToDelete(null);
-        }
-      });
-  }, [itemToDelete]);
-
-  const handleDelete = (value) => () => {
-    console.log(value);
-    fetch(`http://localhost:8080/dashboard/contact/${value}`, { method: "DELETE" })
-      .then(() => {
-        setItemToDelete(value);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const handleToggle = (value) => () => {
-    // logika do zrobienia
-  }
-
-  const handleSearch = (newSearchText) => {
-    setSearchText(newSearchText);
-  };
-
-return (
+export default function ContactEmails() {
+  return (
     <div>
-      <TextFieldName handleSearch={handleSearch} />
-      <SelectType />
-      <TextFieldDescribe/>
+      <Typography variant="h6" gutterBottom>
+        Napisz na wybrany e-mail:
+      </Typography>
 
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle1" gutterBottom>
+            Szymon:
+          </Typography>
+          <Typography variant="body1">szymonpogwizd12@gmail.com</Typography>
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={() => handleOpenMailAppSzymon("szymon@gmail.com")}
+            >
+              Otwórz aplikację poczty
+            </Button>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle1" gutterBottom>
+            Jakub:
+          </Typography>
+          <Typography variant="body1">jakubporeba8@gmail.com</Typography>
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={() => handleOpenMailAppJakub("jakubporeba8@gmail.com")}
+            >
+              Otwórz aplikację poczty
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
     </div>
   );
 }
